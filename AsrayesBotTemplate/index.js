f94cb58f1d64fb820739e56764d3a542a79c13cb
Asraye <asrayeofficial@gmail.com>
2025-07-27 02:41:56 +1000

    Added a cardinal sin (I will update more of the template, soon)

--- AsrayesBotTemplate/index.js
+++ AsrayesBotTemplate/index.js
@@ -60,6 +60,18 @@ client.on("messageCreate", async (msg) => {
   }
 });
 
-
-
 client.loginBot(config.token);
+
+// Please forgive me for this cardinal sin, but, this is the only way to make the damned bot not go down randomly 24/7
+process.on("unhandledRejection", (reason, p) => {
+  console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+  console.log(reason, p);
+});
+process.on("uncaughtException", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+  console.log(err, origin);
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+  console.log(err, origin);
+});
